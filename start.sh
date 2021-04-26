a152c7a860f9fac0bea1cd629f8bc16d0c795c51
test <alecromski@gmail.com>
2021-04-26 11:37:44 +0200

    work on some usefull needed installation and configuration

--- start.sh
+++ start.sh
@@ -5,8 +5,6 @@ GIT_MAIL=''
 GIT_EDITOR='vim'
 GIT_BRANCH='master'
 CONFIG="$HOME/.config"
-DOTFILE="$HOME/GIT/Dotfile/"
-DOTFILE_DIR="$HOME/GIT/DOTFILE/Dotfile"
 
 banner(){
 	cat << "EOF"
@@ -31,31 +29,20 @@ banner(){
 EOF
 }
 
-Dotfile() # clone dotfile where they need to be cloned
-{
-    mkdir -p $HOME/GIT
-    [[ ! -e $HOME/GIT/start-page ]] && git clone https://@github.com/alecromski/start-page $HOME/GIT/start-page
-    [[ ! -e $HOME/Wallpaper ]] && git clone https://@github.com/alecromski/Wallpaper $HOME/Wallpaper
-    [[ ! -e $HOME/Templates ]] && git clone https://@github.com/alecromski/Templates $HOME/Templates
-    [[ ! -e $HOME/GIT/Dotfile ]] && git clone https://@github.com/alecromski/Dotfile $DOTFILE
+AUR(){ # install AUR manager and aur software
+	read -p "do you want to install trizen ? [Y/n]" yn; [[ $yn == y ]] && (git clone https://aur.archlinux.org/trizen /tmp/trizen && cd /tmp/trizen && makepkg -si)
+	read -p "do you want install all AUR package ? [Y/n]" yn ; [ $yn == y ] && trizen -S $(cat "src/aur") && printf "You have install all software from AUR repositories\n"
 }
 
-AUR() # install AUR manager and aur software
-{
-	read -p "do you want to install trizen ? [Y/n]" yn; [[ $yn == y ]] && (git clone https://aur.archlinux.org/trizen && cd trizen && makepkg -si && cd .. && rm -rf trizen)
-	read -p "do you want install all AUR package ? [Y/n]" yn ; [ $yn == y ] && trizen -S $(cat "src/AURInstall") && printf "You have install all software from AUR repositories\n"
-}
-
-PacInstall() # generate pacman mirrorlist blackarch and install all software i need
-{
+PacInstall(){ # generate pacman mirrorlist blackarch and install all software i need
     sudo rm -rf /etc/pacman.conf
     sudo cp src/pacman.conf /etc/pacman.conf
 
     read -p "do you want to automaticaly regenerate pacman depots ? [Y/n]" depots ;	[[ $depots = y ]] && (sudo pacman -S reflector && sudo reflector -c FR -c US -c GB -c PL -n 100 --info --protocol http,https --save /etc/pacman.d/mirrorlist)
 
-	read -p "Do you want to add Blackarch repo ? [Y/n]" black && [[ $black = y ]] && (wget -O /tmp/strap.sh https://blackarch.org/strap.sh && chmod +x /tmp/strap.sh && sudo sh /tmp/strap.sh && sudo rm strap.sh ) 
+	read -p "Do you want to add Blackarch repo ? [Y/n]" black && [[ $black = y ]] && (wget -O /tmp/strap.sh https://blackarch.org/strap.sh && chmod +x /tmp/strap.sh && sudo sh /tmp/strap.sh) 
 
-	read -p "Do you want to install BlackArch software ? [Y/n]" blackarch && [[ $blackarch = y ]] && sudo pacman -S $(cat "src/Blackarch")
+	read -p "Do you want to install BlackArch software ? [Y/n]" blackarch && [[ $blackarch = y ]] && sudo pacman -S $(cat "src/black")
 
 	read -p "Do you want to install some games stations ? [Y/n]" game ; [[ $game = y ]] && trizen -S $(cat src/game)
 
@@ -65,11 +52,10 @@ PacInstall() # generate pacman mirrorlist blackarch and install all software i n
 	
 	sudo pacman -Syy 
 	
-	print "Install Archlinux base software" ; sudo pacman -S $(cat "src/Archlinux")
+	print "Install Archlinux base software" ; sudo pacman -S $(cat "src/arch-base")
 }
 
-GIT() # generate .gitconfig
-{
+GIT(){ # generate .gitconfig
     [[ ! -e $HOME/.gitconfig ]] && read -p "What is your username on GIT server : " GIT_USER && git config --global user.name $GIT_USER && printf "Your username is $GIT_USER\n" &&	read -p "What is your email on GIT server : " GIT_MAIL && git config --global user.email $GIT_MAIL && printf "Your email is $GIT_MAIL\n" && read -p "What is your editor for GIT commit and merge : " GIT_EDITOR &&	git config --global core.editor $GIT_EDITOR && printf "Your editor is $GIT_EDITOR\n" && read -p "How do you want to name your default git branch :" && git config --global init.defaultBranch $GIT_BRANCH && printf "Your default branch is $GIT_BRANCH\n"
 }
 
@@ -82,37 +68,59 @@ DE() # setup DesktopEnvironement
 }
 ##
 
-Zsh() # setup ZSH
-{
-	printf "Configure ZSH" ; cp -r $DOTFILE_DIR/zsh $HOME/.zsh && ln -sf $HOME/.zsh/zshrc $HOME/.zshrc
+epitech(){
+	print "Work in progress n00b"
+	(git clone https://github.com/kayofeld/makegen-file /tmp/ && cd /tmp/makegen-file/ && sudo ./install.sh)
 }
 
-Vim() # setup vim
-{
-    [[ -e $HOME/.vim || -e $HOME/.vimrc ]] && printf "you have already a vim conf" || cp -r $DOTFILE/vim $HOME/.vim && ln -sf $HOME/.vim/vimrc $HOME/.vimrc
+user_manage(){
+	sudo usermod -aG input $USER
+	sudo usermod -aG uucp $USER
+	sudo usermod -aG tty $USER
+	sudo groupadd dialout && sudo usermod -aG dialout $USER
 }
 
-Config()
-{
-	DE
-	Zsh
-	Vim
-}
-
-sysD() # enable system dep
-{
-	sudo systemctl enable cups NetworkManager bluetooth
+sysD(){ # enable system dep
+	sudo systemctl enable cups NetworkManager bluetooth 
 	read -p "What is the Name of your computer ?" STATION && echo $STATION | sudo tee -a /etc/hostname && echo '127.0.0.1\t\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t\t'$STATION | sudo tee -a /etc/hosts
 	sudo hwclock --systohc
 	sudo timedatectl set-ntp true
 	sudo localectl set-keymap fr
 	sudo localectl set-x11-keymap fr
+	sudo chmod +s /sbin/shutdown
+	sudo chmod +s /sbin/reboot
 	(curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/master/scripts/99-platformio-udev.rules | sudo tee /etc/udev/rules.d/99-platformio-udev.rules)
 	python3 -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
-	sudo usermod -aG input $USER
-	sudo usermod -aG uucp $USER
-	sudo usermod -aG tty $USER
-	sudo groupadd dialout && sudo usermod -aG dialout $USER
+	user_manager
+}
+
+SleepClear(){
+	sleep 2
+	clear
+}
+
+first(){ ## install base software
+	banner
+	AUR
+	PacInstall
+}
+
+second(){ ## setup
+	SleepClear
+	GIT
+	SleepClear
+	DE
+	SleepClear
+	epitech
+	SleepClear
+	sysD
+}
+
+third(){
+	(git clone https://github.com/kawaegle/Dotfile/ /tmp/dotfile && cd /tmp/dotfile && ./dotfile restore)  2>&1
+}
+
+finish(){
 	printf "You 'll need to restart soon...\nBut no problem just wait we'll restart it for you.\n"; sleep 2
 	printf "Reboot in 5..."; sleep 1
 	printf "Reboot in 4..."; sleep 1
@@ -123,34 +131,12 @@ sysD() # enable system dep
 	sudo reboot
 }
 
-SleepClear()
-{
-	sleep 2
-	clear
-}
-
-first()
-{
-	banner
-	Dotfile 
-	AUR
-	PacInstall
-}
-
-second(){
-	SleepClear
-	GIT
-	SleepClear
-	Config
-	SleepClear
-	sysD
-}
-
 main(){
 	first
 	read -p "Do you want to continue the configuration ? [Y/n] " yn
 	[[ $yn != 'y' ]] && exit
 	second
+	finish
 }
 
 main
